80806aa3f205cbf990974830368f70d31b4b637d
0324bence <bence20050324@gmail.com>
2023-01-24 19:34:15 +0100

    Progress with local game page

--- src/routes/local/game/+page.svelte
+++ src/routes/local/game/+page.svelte
@@ -6,4 +6,34 @@
     console.log(form);
 </script>
 
-<h1>Game</h1>
+<div id="container">
+    <div id="player-name-container">
+        <span>Player 1</span>
+        <span>Player 2</span>
+    </div>
+</div>
+
+<style lang="scss">
+    @import "@/assets/variables.scss";
+    #container {
+        width: 30%;
+        height: 100vh;
+        padding: 3rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        color: $foreground_color;
+
+        #player-name-container {
+            display: flex;
+            width: 100%;
+            align-items: center;
+            justify-content: space-between;
+
+            span {
+                font-size: 3rem;
+            }
+        }
+    }
+</style>
