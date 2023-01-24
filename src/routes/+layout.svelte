da3a4f9e11465d5b75a61f23bbcef1e5f087864e
0324bence <bence20050324@gmail.com>
2023-01-24 19:33:54 +0100

    Add basic background to all pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<div id="container">
+    <slot />
+</div>
+
+<style lang="scss">
+    @import "@/assets/variables.scss";
+    #container {
+        width: 100%;
+        height: 100vh;
+        background-color: $background_color;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
