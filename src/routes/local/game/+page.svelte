8ffa7d49e11bc157b6aeefbdd8d10534e4aa4e38
0324bence <bence20050324@gmail.com>
2023-01-26 20:09:02 +0100

    Finish local game

--- src/routes/local/game/+page.svelte
+++ src/routes/local/game/+page.svelte
@@ -10,8 +10,8 @@
     let width = 0,
         height = 0,
         goal = 0,
-        rows = "",
-        cols = "";
+        currplayer: 1 | 2 = 1,
+        won = 0;
 
     if (form?.data?.width && form?.data?.goal && form?.data?.height) {
         width = parseInt(form?.data?.width);
@@ -24,42 +24,136 @@
             redirect(302, "/");
         }
     }
-    const board = new Board(width, height, goal);
+    let board = new Board(width, height, goal);
 
-    console.log(board);
+    function TileClick(col: number, row: number) {
+        if (won == 0 && board.table[col][row] == 0) {
+            board.table[col][row] = currplayer;
+            const win = board.CheckForWin([col, row], currplayer);
+            if (!win) {
+                currplayer == 1 ? (currplayer = 2) : (currplayer = 1);
+            } else {
+                won = currplayer;
+            }
+        }
+    }
+
+    function Reset() {
+        board = new Board(width, height, goal);
+        currplayer = 1;
+    }
 </script>
 
 <div id="container">
     <div id="player-name-container">
-        <span>Player 1</span>
-        <span>Player 2</span>
+        <span id="player1" class={currplayer == 1 ? "active" : ""}>Player 1</span>
+        <span id="player2" class={currplayer == 2 ? "active" : ""}>Player 2</span>
     </div>
+    {#if won}
+        <div id="wintext">
+            <h1>{won == 1 ? "Player 1" : "Player 2"} won</h1>
+        </div>
+    {/if}
     <div id="gamearea" style:aspect-ratio={width + " / " + height}>
-        {#each board.table as cols}
+        {#each board.table as cols, colindex}
             <div class="col">
-                {#each cols as tile}
-                    <div class="field" />
+                {#each cols as tile, rowindex}
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <div class="field" on:click={() => TileClick(colindex, rowindex)}>
+                        {#if tile == 1}
+                            <svg
+                                version="1.1"
+                                id="Layer_1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink"
+                                x="0px"
+                                y="0px"
+                                width="50px"
+                                height="50px"
+                                viewBox="0 0 50 50"
+                                enable-background="new 0 0 50 50"
+                                xml:space="preserve"
+                            >
+                                <g id="Layer_3">
+                                    <line
+                                        id="path2"
+                                        fill="none"
+                                        stroke="#000000"
+                                        stroke-width="3"
+                                        stroke-miterlimit="10"
+                                        x1="8.5"
+                                        y1="41.5"
+                                        x2="41.5"
+                                        y2="8.5"
+                                    />
+                                    <line
+                                        id="path3"
+                                        fill="none"
+                                        stroke="#000000"
+                                        stroke-width="3"
+                                        stroke-miterlimit="10"
+                                        x1="41.5"
+                                        y1="41.5"
+                                        x2="8.5"
+                                        y2="8.5"
+                                    />
+                                </g>
+                            </svg>
+                        {/if}
+                        {#if tile == 2}
+                            <svg
+                                version="1.1"
+                                id="circle"
+                                xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink"
+                                x="0px"
+                                y="0px"
+                                width="50px"
+                                height="50px"
+                                viewBox="0 0 100 100"
+                                enable-background="new 0 0 50 50"
+                                xml:space="preserve"
+                            >
+                                <circle
+                                    fill="none"
+                                    stroke="#000"
+                                    stroke-width="5"
+                                    cx="50"
+                                    cy="50"
+                                    r="45"
+                                    stroke-dasharray="360"
+                                    stroke-linecap="round"
+                                    transform="rotate(-90 ) translate(-100 0)"
+                                >
+                                    <animate attributeName="stroke-dashoffset" values="360;0" dur="0.20s" />
+                                </circle>
+                            </svg>
+                        {/if}
+                    </div>
                 {/each}
             </div>
         {/each}
     </div>
-    <div id="buttons" />
+    <div id="buttons">
+        <button class="reset-button" on:click={Reset}>Reset</button>
+    </div>
 </div>
 
 <style lang="scss">
     @import "@/assets/variables.scss";
     #container {
-        width: 30%;
-        height: 100vh;
-        padding: 3rem;
+        width: 40%;
+        min-height: 100vh;
+        padding: 0 3rem 0 3rem;
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: space-between;
+        justify-content: space-around;
         color: $foreground_color;
+        gap: 1rem;
 
         #gamearea {
-            width: 140%;
+            width: 100%;
             display: flex;
             align-items: stretch;
             justify-content: flex-start;
@@ -78,21 +172,103 @@
                 .field {
                     flex: 1;
                     background-color: $foreground_color;
+                    display: grid;
+                    place-items: center;
+                    aspect-ratio: 1 / 1;
                     // width: 100%;
                     // height: 100%;
+
+                    svg {
+                        height: 90%;
+                        width: 90%;
+                    }
+                    #path2 {
+                        stroke-dasharray: 430;
+                        stroke-dashoffset: 800;
+                        animation: x 0.1s linear;
+                        animation-fill-mode: forwards;
+                    }
+                    #path3 {
+                        stroke-dasharray: 430;
+                        stroke-dashoffset: 800;
+                        animation: x 0.1s linear;
+                        animation-fill-mode: forwards;
+                        animation-delay: 0.15s;
+                    }
+                    @keyframes x {
+                        from {
+                            stroke-dasharray: 430;
+                        }
+                        to {
+                            stroke-dasharray: 400;
+                        }
+                    }
                 }
             }
         }
 
         #player-name-container {
             display: flex;
-            width: 100%;
+            flex-wrap: wrap;
+            width: 90%;
             align-items: center;
-            justify-content: space-between;
+            justify-content: space-around;
 
             span {
+                text-align: center;
                 font-size: 3rem;
+                position: relative;
+                overflow: hidden;
             }
+            #player1 {
+                &::after {
+                    content: "";
+                    position: absolute;
+                    bottom: 0;
+                    left: 0;
+                    opacity: 1;
+                    width: 100%;
+                    height: 5%;
+                    background-color: $foreground_color;
+                    transition: transform 0.2s;
+                    transform: translateX(-105%);
+                }
+                &.active::after {
+                    transform: translateX(0);
+                }
+            }
+            #player2 {
+                &::after {
+                    content: "";
+                    position: absolute;
+                    bottom: 0;
+                    left: 0;
+                    opacity: 1;
+                    width: 100%;
+                    height: 5%;
+                    background-color: $foreground_color;
+                    transition: transform 0.2s;
+                    transform: translateX(105%);
+                }
+                &.active::after {
+                    transform: translateX(0);
+                }
+            }
+        }
+
+        #buttons {
+            .reset-button {
+                @include button;
+                width: 20rem;
+                border: none;
+                outline: none;
+            }
+        }
+    }
+
+    @media screen and (max-width: 768px) {
+        #container {
+            width: 100%;
         }
     }
 </style>
--- src/routes/local/game/game.ts
+++ src/routes/local/game/game.ts
@@ -1,5 +1,5 @@
 export class Board {
-    public table: Array<Array<number>>;
+    public table: Array<Array<0 | 1 | 2>>;
     constructor(private width: number, private height: number, private goal: number) {
         this.table = new Array(width);
         for (let i = 0; i < this.table.length; i++) {
@@ -11,8 +11,63 @@ export class Board {
         }
     }
 
-    CheckForWin(): boolean {
-        /* TODO */
-        return false;
+    CheckForWin(from: [number, number], player: 1 | 2): boolean {
+        const column = from[0];
+        const row = from[1];
+        let win = false,
+            counter = 0;
+
+        for (let i = 0; i < this.height; i++) {
+            if (this.table[column][i] == player) counter++;
+            else counter = 0;
+            if (counter >= this.goal) {
+                win = true;
+                break;
+            }
+        }
+        if (win) return win;
+        counter = 0;
+        for (let i = 0; i < this.width; i++) {
+            //console.log(`${this.table[i][row]} == ${player}, row: ${row} col: ${i}`);
+            if (this.table[i][row] == player) {
+                counter++;
+                //console.log(`counter up ${player}:${counter}`);
+            } else {
+                counter = 0;
+            }
+            if (counter >= this.goal) {
+                win = true;
+                break;
+            }
+        }
+        if (win) return win;
+        counter = 0;
+        let startpoint: [number, number] = [0, 0];
+        for (let col = column, ro = row; col >= 0 && ro >= 0; col--, ro--) {
+            startpoint = [col, ro];
+        }
+        for (let [col, ro] = startpoint; col < this.width && ro < this.height; col++, ro++) {
+            if (this.table[col][ro] == player) counter++;
+            else counter = 0;
+            if (counter >= this.goal) {
+                win = true;
+                break;
+            }
+        }
+        if (win) return win;
+        counter = 0;
+        for (let col = column, ro = row; col < this.width && ro >= 0; col++, ro--) {
+            startpoint = [col, ro];
+        }
+        for (let [col, ro] = startpoint; col >= 0 && ro < this.height; col--, ro++) {
+            if (this.table[col][ro] == player) counter++;
+            else counter = 0;
+            if (counter >= this.goal) {
+                win = true;
+                break;
+            }
+        }
+
+        return win;
     }
 }
